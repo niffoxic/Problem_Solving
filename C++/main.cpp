61d7ba42a128f5b128096c5bb9b40082df834052
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-06-14 05:44:59 +0530

    Create main.cpp

---
+++ C++/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using str = string;
+using db = double;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pdb = pair<db, db>;
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<db>;
+using vs = vector<str>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vpd = vector<pdb>;
+
+#define mp make_pair
+#define f first
+#define s second
+#define pb push_back
+#define lb lower_bound
+
+const int mod = 1e9+7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t;
+
+    while(t--){
+
+    }
+    return 0;
+}
