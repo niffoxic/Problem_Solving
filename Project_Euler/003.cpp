cfc47c4ea1718964024a1e173994d5d9f2db7275
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-06-19 19:28:42 +0530

    Create 003.cpp

---
+++ Project_Euler/003.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+using ll = long long;
+using str = string;
+using db = long double;
+using ull = unsigned long long;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pdb = pair<db, db>;
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<db>;
+using vs = vector<str>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vpd = vector<pdb>;
+
+#define mp make_pair
+#define f first
+#define s second
+#define pb push_back
+#define lb lower_bound
+#define up upper_bound
+
+const int mod = 1e9+7;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    //cin >> t;
+
+    while(t--) {
+        ull n;
+        cin >> n;
+
+        while (!(n % 2)) n /= 2;
+        int diff = 3;
+        while (diff * diff <= n){
+            if (!(n % diff)) n /= diff;
+            else diff += 2;
+        }
+        cout << ((n == 1)? 2: n) << "\n";
+    }
+
+    return 0;
+}
