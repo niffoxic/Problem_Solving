2d29ea468aa155a25598e5cc37f1ce47850def93
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-06-20 12:31:42 +0530

    Create 01_Weird_Algorithm.cpp

---
+++ CSES/01_Introductory_Problems/01_Weird_Algorithm.cpp
@@ -0,0 +1,16 @@
+using ll = long long;
+
+int main(){
+  ll n;
+  cin >> n;
+
+  while (n > 1)
+  {
+      cout << n << " ";
+      if (n & 1)n = 3 * n + 1;
+      else n /= 2;
+  }
+  
+  cout << 1;
+  return 0;
+}
