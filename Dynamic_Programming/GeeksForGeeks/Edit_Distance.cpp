14dab4fc19a8c1c3a30b501533a96a9ae8f5ebc5
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-06-29 04:57:45 +0530

    Update Edit_Distance.cpp

--- Dynamic_Programming/GeeksForGeeks/Edit_Distance.cpp
+++ Dynamic_Programming/GeeksForGeeks/Edit_Distance.cpp
@@ -16,3 +16,30 @@ int edit_distance(str s1, str s2){
     }
     return solve_ed(s1, s1.size(), s2, s2.size());
 }
+
+
+// Bottom up DP
+
+int editDistance2(string s1, string s2){
+    int dp4[101][101];
+    int n = s1.size(), m = s2.size();
+    for(int i = 0; i <= m; i++) dp4[0][i] = i;
+    for(int i = 0; i <= n; i++) dp4[i][0] = i;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if (s1[i - 1] == s2[j - 1]){
+                dp4[i][j] = dp4[i - 1][j - 1];
+            }else{
+                dp4[i][j] = 1 + min(dp4[i - 1][j], min(dp4[i - 1][j - 1], dp4[i][j - 1]));
+            }
+        }
+    }
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <= m; j++){
+            cout << dp4[i][j] << " ";
+        }
+        print("")
+    }
+    return dp4[n][m];
+}
