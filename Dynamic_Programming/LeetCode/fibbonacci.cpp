a0984c484c36e5c7ed6d9686bff806e57e1dfcda
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-06-28 15:07:17 +0530

    Create fibbonacci.cpp

---
+++ Dynamic_Programming/LeetCode/fibbonacci.cpp
@@ -0,0 +1,10 @@
+// can be optimsed easily but soo basic question so I didnt bother to do that
+using ull = unsigned long long;
+
+ull fib(ull n, ull dp[]){
+    if (n == 1 || n == 2) return 1;
+    if (n == 0) return 0;
+    if (dp[n]) return dp[n];
+    ull ans = fib(n - 1, dp) + fib(n - 2, dp);
+    return dp[n] = ans;
+}
