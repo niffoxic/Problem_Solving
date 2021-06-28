4e34875f2a2db3c14a4bbacd3f8665aeac6cbdac
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-06-28 19:06:16 +0530

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -11,6 +11,8 @@
 #include <deque>
 #include <set>
 #include <algorithm>
+#include <iomanip>
+
 using namespace std;
 
 using ll = long long;
@@ -30,6 +32,10 @@ using vs = vector<str>;
 using vpi = vector<pii>;
 using vpl = vector<pll>;
 using vpd = vector<pdb>;
+using umi = unordered_map<int, int>;
+
+#define print(x) cout << x << nn;
+#define all(x) x.begin(), x.end()
 
 #define mp make_pair
 #define f first
@@ -37,21 +43,37 @@ using vpd = vector<pdb>;
 #define pb push_back
 #define lb lower_bound
 #define up upper_bound
+#define nn "\n"
+#define Yes "Yes"
+#define YES "YES"
+#define yes "yes"
+#define no "no"
+#define No "No"
+#define NO "NO"
+#define def auto
+
+// ****************************************************************************************
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v);
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v);
+template <typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &v);
+template <typename T, typename S> ostream &operator<<(ostream &os, const pair<T, S> &v);
+template <typename T> inline istream &operator>>(istream &in, vector<T> &a);
+// ****************************************************************************************
 
 const int mod = 1e9+7;
 
 int main()
 {
+//  freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     int t = 1;
-    //cin >> t;
-
-    while(t--) {
-        ull n;
-        cin >> n;
-
+    // cin >> t;
+    while(t--)
+    {
+        int n; cin >> n;
+     
     }
-
     return 0;
 }
