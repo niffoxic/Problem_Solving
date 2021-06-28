387e395243e6d8860bc85ec2ddb42cfe1420ce62
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-06-29 01:00:16 +0530

    Update Coin_Change.cpp

--- Dynamic_Programming/GeeksForGeeks/Coin_Change.cpp
+++ Dynamic_Programming/GeeksForGeeks/Coin_Change.cpp
@@ -1,6 +1,6 @@
 
 long long dp[1001][1001];
-
+// Top bottom DP
 long long int rcount(int const coins[], int m, int n){
     if (n == 0) return 1; if (m == 0) return 0;
     if (dp[n][m]) return dp[n][m];
