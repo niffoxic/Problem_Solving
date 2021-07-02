f41e5e764fce1cd89a650b8d5b3f93843833b554
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-07-02 23:48:25 +0530

    Create Longest_Palindromic_Subsequence.cpp

---
+++ Dynamic_Programming/GeeksForGeeks/Longest_Palindromic_Subsequence.cpp
@@ -0,0 +1,21 @@
+int dp[1001][1001];
+
+int longestPalinSubseq (string s1)
+{
+    string s2 = s1;
+    reverse(s2.begin(), s2.end());
+    int n = s1.size();
+    for(int i = 0; i <= n; i++){
+        dp[0][i] = 0;
+        dp[i][0] = 0;
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if (s1[i - 1] == s2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+            else{
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[n][n];
+}
