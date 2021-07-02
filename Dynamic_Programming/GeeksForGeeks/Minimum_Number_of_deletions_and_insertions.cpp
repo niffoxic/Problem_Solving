3240590594500f09588fcd95b2a1daa8651b3a7a
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-07-02 23:17:10 +0530

    Create Minimum_Number_of_deletions_and_insertions.cpp

---
+++ Dynamic_Programming/GeeksForGeeks/Minimum_Number_of_deletions_and_insertions.cpp
@@ -0,0 +1,21 @@
+// Minimum number of deletions and insertions
+
+int minOperations(string s1, string s2){
+    int n = s1.size(), m = s2.size();
+
+    for(int i = 0; i <= n; i++) dp[i][0] = 0;
+    for(int j = 0; j <= m; j++) dp[0][j] = 0;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if (s1[i - 1] == s2[j - 1]){
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }else{
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    int res = n - dp[n][m];
+    res += m - dp[n][m];
+    return res;
+}
