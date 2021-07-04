dbd65fd26ad754cc0982a72cdc371229694ee477
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-07-04 10:25:23 +0530

    Create Form_a_Palindrome.cpp

---
+++ Dynamic_Programming/GeeksForGeeks/Form_a_Palindrome.cpp
@@ -0,0 +1,22 @@
+// DP
+int countMin(string s1){
+    string s2 = s1;
+    reverse(s2.begin(), s2.end());
+    
+    int n = s2.length();
+    int dp[n + 1][n + 1];
+    
+    for(int i = 0 ; i <= n; i++){
+        dp[i][0] = 0;
+        dp[0][i] = 0;
+    }
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if (s1[i - 1] == s2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+            else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    
+    return n - dp[n][n];
+}
