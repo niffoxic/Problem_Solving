abcdba370caef13ea4bb716d4b6e2d5321aa98f8
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-07-04 19:56:55 +0530

    Update Palindromic_Partition.cpp

--- Dynamic_Programming/GeeksForGeeks/Palindromic_Partition.cpp
+++ Dynamic_Programming/GeeksForGeeks/Palindromic_Partition.cpp
@@ -1,5 +1,4 @@
 int dp[501][501];
-
 bool isPalindromic(string &s1, int i, int j){
     while (i < j){
         if (s1[i] != s1[j]) return false;
@@ -17,7 +16,9 @@ int solve(string &s1, int i, int j){
 
     int res = INT_MAX;
     for(int k = i; k < j; k++){
-        int current = solve(s1, i, k) + solve(s1, k + 1, j) + 1;
+        int current = 1;
+        current += dp[i][k] == -1? solve(s1, i, k): dp[i][k];
+        current += dp[k + 1][j] == -1? solve(s1, k + 1, j): dp[k + 1][j];
         res = min(res, current);
     }
 
@@ -28,4 +29,5 @@ int palindromicPartition(string &s1)
 {
     for(int i = 0; i <= s1.length(); i++) for(int j = 0; j <= s1.length(); j++) dp[i][j] = -1;
     return solve(s1, 0, s1.length() - 1);
+
 }
