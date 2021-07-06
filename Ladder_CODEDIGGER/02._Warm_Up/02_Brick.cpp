85a3fb7ea0ad55754df493b442b927ae05d338a9
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-07-06 21:14:09 +0530

    Create 02_Brick.cpp

---
+++ Ladder_CODEDIGGER/02._Warm_Up/02_Brick.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+    //  freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int test_case = 1;
+    // cin >> test_case;
+    while (test_case--)
+    {
+        int n, w; cin >> n >> w; cout << n / w << nn;
+    }
+
+    return 0;
+}
