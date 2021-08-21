8c7d9d9a1522e7ed4fdacdfd66021d4720016022
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-08-21 17:01:17 +0530

    Create Largest_substring.cpp

---
+++ Hackerearth_Solutions/Algorithms/Binary_Search/Largest_substring.cpp
@@ -0,0 +1,23 @@
+void solve(int case_no)
+{
+
+    int n; cin >> n;
+    str s1; cin >> s1;
+    umii res;
+    int ans = 0, val = 0, curr = 0;
+    int counts = 0;
+    for(int i = 0; i < n; i++){
+        if (s1[i] == '0') counts++;
+        else counts--;
+        if (counts > 0){
+            ans = i + 1;
+        }else{
+            if(res.find(counts - 1) != res.end()){
+                ans = max(ans, i - res[counts - 1]);
+            }
+        }
+        if (res.find(counts) == res.end())
+            res[counts] = i;
+    }
+    cout << ans << nn;
+}
