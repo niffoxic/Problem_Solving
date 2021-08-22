b8ed4d5743547615867868a73526bd0b54b44af3
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-08-23 04:22:09 +0530

    Create compare_String.cpp

---
+++ Hackerearth_Solutions/Algorithms/Binary_Search/compare_String.cpp
@@ -0,0 +1,18 @@
+void solve(int case_no){
+    int n, q; cin >> n >> q;
+    int ac = 0, bc = 0;
+    str s1, s2; cin >> s1 >> s2;
+    for(int i = 0; i < n; i++){
+        if (s1[i] == '1') ac++;
+        if (s2[i] == '1') bc++;
+    }
+    for(int i = 0; i < q; i++)
+    {
+        int k; cin >> k;
+        if (s2[k - 1] != '1'){
+            s2[k - 1] = '1';
+            bc++;
+        }
+        cout << (bc == ac?YES:NO) << nn;
+    }
+}
