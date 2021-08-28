421686534aaa94d265bddb216752ab8204ae1cad
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-08-28 20:59:35 +0530

    Create Counting_triangles.cpp

---
+++ Hackerearth_Solutions/Algorithms/Binary_Search/Counting_triangles.cpp
@@ -0,0 +1,18 @@
+void solve(int case_no){
+    int n; cin >> n;
+    map<str, ll> res;
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        ll arr[3]; cin >> arr[0] >> arr[1] >> arr[2];
+        sort(arr, arr + 3);
+        str s1 = to_string(arr[0]) + to_string(arr[1]) + to_string(arr[2]);
+        if (res.find(s1) != res.end() && res[s1] == 1){
+            res[s1] = 0;
+            ans--;
+        }
+        if (res.find(s1) == res.end()){
+            ans++; res[s1]++;
+        }
+    }
+    print(ans)
+}
