92b93e5fb24b4433b283e3cf513b739fe0b9b5e5
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-08-28 23:46:18 +0530

    Create intersection_of_two_arrays.cpp

---
+++ Hackerearth_Solutions/Algorithms/Binary_Search/intersection_of_two_arrays.cpp
@@ -0,0 +1,24 @@
+#define pb push_back
+#define all(x) x.begin(), x.end()
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> *search, *other;
+        sort(all(nums1)), sort(all(nums2));
+        if (nums1.size() > nums2.size()){
+            search = &nums1;
+            other = &nums2;
+        }else {
+            search = &nums2;
+            other = &nums1;
+        }
+        unordered_set<int>res;
+        for(auto i:*search){
+            if (binary_search(other->begin(), other->end(), i)) res.insert(i);
+        }
+        vector<int>ans;
+        for(auto i: res) ans.pb(i);
+        return ans;
+    }
+};
