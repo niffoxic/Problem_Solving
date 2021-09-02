a4552442a6958c83a82bb780b52580660ca6daa9
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-01 19:46:04 -0700

    Create search_in_rotated_and_sorted_array.cpp

---
+++ Hackerearth_Solutions/Algorithms/Binary_Search/search_in_rotated_and_sorted_array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while(left <= right){
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] >= nums[left]){
+                if (nums[mid] >= target && target >= nums[left]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }else{
+                if (nums[mid] <= target && target <= nums[right]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
