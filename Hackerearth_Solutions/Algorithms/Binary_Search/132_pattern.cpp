6ba1fbbf9de9942e76efe5dc9a6c631e177f6513
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-07 03:24:07 -0700

    Create 132_pattern.cpp

---
+++ Hackerearth_Solutions/Algorithms/Binary_Search/132_pattern.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        if (nums.size() < 3) return false;
+        set<int> right;
+        vector<int> left(nums.size() + 1);
+        left[0] = nums[0];
+        for(int i = 1; i < nums.size(); i++)
+            left[i] = min(left[i - 1], nums[i]);
+        right.insert(nums[nums.size() - 1]);
+        for(int i = nums.size() - 2; i >= 1; i--){
+            if (nums[i] > left[i - 1]){
+                auto rhs = right.upper_bound(left[i - 1]);
+                if (rhs != right.end()){
+                    if (*rhs < nums[i])
+                        return true;
+                }
+            }
+            right.insert(nums[i]);
+        }
+        return false;
+    }
+};
