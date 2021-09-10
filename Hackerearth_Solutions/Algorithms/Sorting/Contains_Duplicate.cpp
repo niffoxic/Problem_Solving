bf2bb245235b7b94e405d2d6b852c06d04cec0cb
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-10 10:24:13 -0700

    Create Contains_Duplicate.cpp

---
+++ Hackerearth_Solutions/Algorithms/Sorting/Contains_Duplicate.cpp
@@ -0,0 +1,25 @@
+
+// Solution 1 - Using HashMap
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int>res;
+        for(auto i: nums){
+            if (res.find(i) != res.end()) return true;
+            res[i]++;
+        }
+        return false;
+    }
+};
+
+// Solution 2- using sorting
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 1; i++){
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
+};
