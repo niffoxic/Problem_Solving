4533af1db246e0d7b7fc8c32ba331f882f82b1ab
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-11 10:36:49 -0700

    Create maximum_product_of_three_number.cpp

---
+++ Hackerearth_Solutions/Algorithms/Sorting/maximum_product_of_three_number.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return max(nums[0] * nums[1] * nums[nums.size() - 1], nums[nums.size() - 1] * nums[nums.size() - 2] * nums[nums.size() - 3]);
+    }
+};
