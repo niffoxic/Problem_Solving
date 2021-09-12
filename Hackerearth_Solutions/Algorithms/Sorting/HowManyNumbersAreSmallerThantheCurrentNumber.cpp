433e4c8dd36b0d4186fd4720a3e451a4641229c9
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-12 11:05:41 -0700

    Update HowManyNumbersAreSmallerThantheCurrentNumber.cpp

--- Hackerearth_Solutions/Algorithms/Sorting/HowManyNumbersAreSmallerThantheCurrentNumber.cpp
+++ Hackerearth_Solutions/Algorithms/Sorting/HowManyNumbersAreSmallerThantheCurrentNumber.cpp
@@ -24,3 +24,28 @@ public:
         return ans;
     }
 };
+
+// Solution 2
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        unordered_map<int,int> s;
+        vector<int> a;
+        for(int i=0;i<nums.size();i++)
+        {
+            a.push_back(nums[i]);
+        }
+        sort(nums.begin(),nums.end());
+        int x = nums.size();
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            x= x-1;
+            s[nums[i]]= x;
+        }
+        for(int i=0;i<nums.size();i++){
+            nums[i]= s[a[i]];
+        }
+        return nums;
+    }
+};
