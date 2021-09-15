d267c5d1d167ca43b1412724eeb44239655bd0c1
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-15 13:23:14 -0700

    Create minimum_moves_to_equal_array_elements_2.cpp

---
+++ Hackerearth_Solutions/Algorithms/Sorting/minimum_moves_to_equal_array_elements_2.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int mark = nums.size() / 2;
+        for(int i = 0; i < nums.size(); i++)
+            ans += abs(nums[i] - nums[mark]);
+        return ans;
+    }
+};
