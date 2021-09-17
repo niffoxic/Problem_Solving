b950d51f8f316dbd240c62704156f820fe79922e
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-17 14:24:03 -0700

    Create bubbleSort.cpp

---
+++ algoexperts/sorting/bubbleSort.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+vector<int> bubbleSort(vector<int> arr) {
+  // Write your code here.
+	for (int i = 0; i < n; i++){
+		for(int j = 0; j < n - i - 1; j++){
+			if (arr[j] > arr[j + 1]){
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+  return arr;
+}
