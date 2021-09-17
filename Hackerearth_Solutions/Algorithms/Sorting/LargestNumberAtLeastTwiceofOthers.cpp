8d67a7596634ebfec593871ec431d1a173148d10
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-17 13:58:21 -0700

    Update LargestNumberAtLeastTwiceofOthers.cpp

--- Hackerearth_Solutions/Algorithms/Sorting/LargestNumberAtLeastTwiceofOthers.cpp
+++ Hackerearth_Solutions/Algorithms/Sorting/LargestNumberAtLeastTwiceofOthers.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int dominantIndex(vector<int>& nums) {
-        vector<pair<int, int>> res;
-        for(int i = 0; i < nums.size(); i++){
-            res.push_back(make_pair(nums[i], i));
+        int index = 0, index1 = -1;
+        for(int i = 1; i < nums.size(); i++){
+            if (nums[index] < nums[i]){
+                index1 = index;
+                index = i;
+            }else if (index1 == -1 || nums[index1] < nums[i]){
+                index1 = i;
+            }
         }
-        sort(res.begin(), res.end());
-        if (nums.size() == 1) return 0;
-        return ((res[res.size() - 2].first * 2) <= res[res.size() - 1].first)?res[res.size() - 1].second:-1;
+        if (index1 == -1) return index;
+        return nums[index1] * 2 <= nums[index]?index:-1;
     }
 };
