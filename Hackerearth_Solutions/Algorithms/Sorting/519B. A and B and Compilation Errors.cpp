d34a93ce604ff25ace5ad8606363cebcc9cb4efd
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-19 11:02:13 -0700

    Update 519B. A and B and Compilation Errors.cpp

--- Hackerearth_Solutions/Algorithms/Sorting/519B. A and B and Compilation Errors.cpp
+++ Hackerearth_Solutions/Algorithms/Sorting/519B. A and B and Compilation Errors.cpp
@@ -1,4 +1,4 @@
-void solve(int case_no){
+void solve(int case_no){ // thru hashin
     int n; cin >> n;
     umi a1, a2;
     for(int i = 0; i < n; i++){
@@ -20,4 +20,16 @@ void solve(int case_no){
     for(auto i: a2) if (i.second > 0){
         cout << i.first << nn; break;
     }
+}
+
+void solve2(int case_no){
+    int n; cin >> n;
+    int s1[3] = {0, 0, 0};
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < n - i; j++){
+            int t; cin >> t;
+            s1[i] += t;
+        }
+    }
+    cout << s1[0] - s1[1] << nn << s1[1] - s1[2] << nn;
 }
