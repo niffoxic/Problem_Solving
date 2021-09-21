bca0f5fadc14c8739feca10a8df50ed12342814a
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-21 11:38:52 -0700

    Create binaryTreePreorderTraversal.cpp

---
+++ Hackerearth_Solutions/Algorithms/BST/binaryTreePreorderTraversal.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> res;
+    void ans(TreeNode* root){
+        if (!root) return;
+        res.push_back(root->val);
+        ans(root->left);
+        ans(root->right);
+    }
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        ans(root);
+        return res;
+    }
+};
