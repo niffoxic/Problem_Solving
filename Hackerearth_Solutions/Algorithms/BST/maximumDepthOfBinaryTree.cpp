c6bcfe33e661776cafc8ea05036c06a9b85d4bd5
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-21 11:10:07 -0700

    Create maximumDepthOfBinaryTree.cpp

---
+++ Hackerearth_Solutions/Algorithms/BST/maximumDepthOfBinaryTree.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root, int val=0) {
+        if (!root) return val;
+        return max(maxDepth(root->left, val + 1), maxDepth(root->right, val + 1));
+    }
+};
