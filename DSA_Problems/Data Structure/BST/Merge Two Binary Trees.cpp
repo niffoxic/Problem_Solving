aefdb7ac6bed48844045032c1cad852428efb00e
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-23 13:15:38 -0700

    Create Merge Two Binary Trees.cpp

---
+++ DSA_Problems/Data Structure/BST/Merge Two Binary Trees.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if (!root1 && !root2) return nullptr;
+        if (!root2) return root1;
+        if(!root1) return root2;
+        TreeNode* node = new TreeNode(root1->val + root2->val);
+        node->left = mergeTrees(root1->left, root2->left);
+        node->right = mergeTrees(root1->right, root2->right);
+        return node;
+    }
+};
