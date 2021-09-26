f6234ccb7748a7f83f648878cdb8be8401059752
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-25 23:49:11 -0700

    Create Best Time to Buy and Sell Stock II.cpp

---
+++ DSA_Problems/Algorithms/Greedy/LeetCode/Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    
+int maxProfit(vector<int>& prices){
+    int n = prices.size();
+    int res = 0;
+    for(int i = 1; i < n; i++){
+        res += max(0, prices[i] - prices[i - 1]);
+    }
+    return res;
+}
+};
