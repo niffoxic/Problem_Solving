0a229c2e49e493fab93dfebc3556b90fe39ea9dc
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-26 00:08:07 -0700

    Create containerWithMostWater.cpp

---
+++ DSA_Problems/Algorithms/Greedy/LeetCode/containerWithMostWater.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int res = 0;
+
+        while(left < right){
+            res = max(res, (right - left) * min(height[left], height[right]));
+            if (height[left] <= height[right])
+                left++;
+            else right--;
+        }
+        return res;
+    }
+};
