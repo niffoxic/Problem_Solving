30aa7f4dc102370fcb679047f087c59fdedf97a6
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2021-09-26 23:29:02 -0700

    Create IncreasingTripletSubsequence.cpp

---
+++ DSA_Problems/Algorithms/Greedy/LeetCode/IncreasingTripletSubsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int left = INT_MAX, mid = INT_MAX;
+        for(int i = 0; i < nums.size(); i++){
+            if (nums[i] > mid) return true;
+            else if (nums[i] < left) left = nums[i];
+            else if (nums[i] > left && nums[i] < mid) mid = nums[i];
+        }
+        return false;
+    }
+};
