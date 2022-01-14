6577ac13c5a5c8f249ac16e2e8a66d5cbe220443
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-01-14 06:05:42 +0530

    Create Base numbers.cpp

---
+++ HackerEarth/Algorithms/Binary Search/Base numbers.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+
+ll check(ll mid, ll a, ll b){
+    return 1 + log(a)/log(b)+ mid * (log(mid)/log(b));
+}
+
+
+int main()
+{
+    ll a, n, b;
+    while(cin >> a >> n >> b){
+        ll left = 1, right = n * b;
+        ll res = INT_MAX;
+        while (left <= right){
+            ll mid = left + (right - left) / 2;
+            ll current = check(mid, a, b);
+            if (current == n){
+                res = mid;
+                break;
+            }else if (current > n){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        int c = 0;
+        left = res, right = res;
+
+        while(true){
+            if (check(left--, a, b) == n){
+                c++;continue;
+            }
+            if (check(++right, a, b) == n){
+                c++;
+                continue;
+            }
+            break;
+        }
+        cout << c << "\n";
+    }
+    return 0;
+}
