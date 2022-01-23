020b8064a7a80c1dc6f3bf322e6b4d37dec4d6a8
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-01-23 22:18:49 +0530

    Create Median Game.cpp

---
+++ HackerEarth/Algorithms/Merge Sort/Median Game.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--)
+    {
+        int n; cin >> n; int arr[n];
+        for(int i = 0; i < n; i++) cin >> arr[i];
+        sort(arr, arr + n);
+        cout << arr[0] + arr[n -1] << "\n";
+    }
+    return 0;
+}
