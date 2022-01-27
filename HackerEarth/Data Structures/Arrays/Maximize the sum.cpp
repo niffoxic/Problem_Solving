fb14641e4a7d384b313bf167fdde3d1ba9264cfe
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-01-27 22:20:25 +0530

    Update Maximize the sum.cpp

--- HackerEarth/Data Structures/Arrays/Maximize the sum.cpp
+++ HackerEarth/Data Structures/Arrays/Maximize the sum.cpp
@@ -1,26 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     int t; cin >> t;
     while(t--){
-        unordered_map<long long, long long> mp;
-        long long n, k; cin >> n >> k;
+        unordered_map<ll, ll> mp;
+        ll n, k; cin >> n >> k;
         for(int i = 0; i < n; i++){
             int c; cin >> c;
             mp[c]+=c;
         }
-        vector<long long>res;
+        vector<ll>res;
         for(auto i: mp){
             if (i.second > 0)
                 res.push_back(i.second);
         }
         sort(res.begin(), res.end(), greater<>());
-        long long ans = 0;
-        for(int i = 0; i < min(k, (long long)(res.size())); i++)
+        ll ans = 0;
+        for(int i = 0; i < min(k, (ll)(res.size())); i++)
             ans += res[i];
         cout << ans << "\n";
     }
