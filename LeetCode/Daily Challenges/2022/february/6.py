e85875939469ddd521717c40ba4109965987e5a8
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-02-07 07:33:58 +0530

    Create 6.py

---
+++ LeetCode/Daily Challenges/2022/february/6.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        j = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[j - 2]:
+                nums[j] = nums[i]
+                j += 1
+        return j
