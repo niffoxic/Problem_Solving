00ac1af11e0b2fcdefbb649c359216c9d4e5a7ea
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-02-14 12:34:50 +0530

    Create Robotic moves.cpp

---
+++ HackerEarth/Basic Programming/Basics of Implementation/Robotic moves.cpp
@@ -0,0 +1,86 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <set>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+ 
+using namespace std;
+ 
+using ll = long long;
+using str = string;
+using db = long double;
+using ull = unsigned long long;
+ 
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdb = pair<db, db>;
+ 
+using vi = vector<int>;
+using vb = vector<bool>;
+using vll = vector<ll>;
+using vd = vector<db>;
+using vs = vector<str>;
+using vpi = vector<pii>;
+using vpl = vector<pll>;
+using vpd = vector<pdb>;
+using umi = unordered_map<int, int>;
+using uml = unordered_map<ll, ll>;
+ 
+#define print(x) cout << x << nn;
+#define all(x) x.begin(), x.end()
+ 
+#define mp make_pair
+#define f first
+#define s second
+#define pb push_back
+#define lb lower_bound
+#define up upper_bound
+#define nn "\n"
+#define Yes "Yes"
+#define YES "YES"
+#define yes "yes"
+#define no "no"
+#define No "No"
+#define NO "NO"
+#define def auto
+ 
+// ****************************************************************************************
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v);
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v);
+template <typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &v);
+template <typename T, typename S> ostream &operator<<(ostream &os, const pair<T, S> &v);
+template <typename T> inline istream &operator>>(istream &in, vector<T> &a);
+// ****************************************************************************************
+const int mod = 1e9+7;
+ 
+int powers(int n, int k){
+    if (k == 0) return n;
+    int res = n * n * n;
+    return powers(res, k - 1);
+}
+ 
+int main()
+{
+    //  freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int test_case = 1;
+    cin >> test_case;
+    while (test_case--)
+    {
+        ll n; cin >> n; print(n * (n + 1))
+    }
+ 
+    return 0;
+}
