48b6a5a78ab0bafe5e37d7f84330bb90ea9af427
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-02-15 23:04:14 +0530

    Create Reachable Numbers.cpp

---
+++ The Ultimate Topic List/Basics/[S1] Greed is good, sort of/[EP 1] Implementation/Reachable Numbers.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+ 
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define lb lower_bound
+#define up upper_bound
+ 
+ 
+const int mod = 1e9 + 7;
+const int dp_size = 1e5;
+const ll INF = LLONG_MAX;
+ 
+ll n, m, q, k, l, r, x, y, z, w, h, res;
+const ll template_array_size = 1e6;
+ll a[template_array_size];
+ll b[template_array_size];
+ll c[template_array_size];
+string s, t;
+ 
+void f(){
+    n++;
+    while (!(n % 10)) n /= 10;
+}
+ 
+void solve(int case_no)
+{
+    unordered_set<ll> st;
+    cin >> n;
+    while (st.find(n) == st.end()) st.insert(n), f();
+    cout << st.size();
+}
+ 
+ 
+int main()
+{
+ 
+//#ifndef ONLINE_JUDGE
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//#endif
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int test_case = 1;
+    // cin >> test_case;
+    for(int i = 1; i <= test_case; i++) solve(i);
+ 
+    return 0;
+}
