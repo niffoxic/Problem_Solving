21606f3bf93e2d6e728b9d44f9c0ebdd6433ec27
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-02-17 15:57:22 +0530

    Create Monk and Tasks.cpp

---
+++ HackerEarth/Basic Programming/Basics of Bit Manipulation/Monk and Tasks.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define lb lower_bound
+#define up upper_bound
+
+
+const int mod = 1e9 + 7;
+const int dp_size = 1e5;
+const ll INF = LLONG_MAX;
+
+ull n, m, q, k, l, r, x, y, z, w, h, total;
+const ll template_array_size = 1e6;
+ull a[template_array_size];
+ll b[template_array_size];
+ll c[template_array_size];
+vector<ll> res[65];
+string s, t;
+
+
+
+void solve(int case_no)
+{
+    cin >> n;
+    r = 0;
+    for(auto & re : res) re.clear();
+    for(int i = 0 ; i < n; i++){
+        cin >> a[i];
+        x = a[i], y = 0;
+        while (x) x &= (x - 1), y++;
+        res[y].pb(a[i]); r = max(r, y);
+    }
+    for(int i = 0; i <= r; i++) for(auto j: res[i]) cout << j << " ";
+    cout << "\n";
+}
+
+int main()
+{
+
+//#ifndef ONLINE_JUDGE
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int test_case = 1;
+    cin >> test_case;
+    for(int i = 1; i <= test_case; i++) solve(i);
+
+    return 0;
+}
