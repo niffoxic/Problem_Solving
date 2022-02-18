d031ef1f6078654676614e4c0654f575fb424401
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-02-18 21:13:16 +0530

    Create Labs.cpp

---
+++ The Ultimate Topic List/Basics/[S1] Greed is good, sort of/[EP 2] Greedy/Labs.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+#define all(x) x.begin(), x.end()
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define up upper_bound
+
+
+const int mod = 1e9 + 7;
+const int dp_size = 1e5;
+const ll INF = LLONG_MAX;
+
+ll n, m, q, k, l, r, x, y, z, c;
+const ll template_array_size = 1e6;
+ll arr[template_array_size];
+ll brr[template_array_size];
+ll crr[template_array_size];
+
+ll Arr[1001][1001];
+ll Brr[1001][1001];
+ll Crr[1001][1001];
+
+string s, t;
+
+void solve(int case_no)
+{
+    cin >> n;
+    for(ll i = 1; i <= n; i++)for(int j = 0; j < n; j++) cout << (!(j & 1)? i + j * n: (j + 1) * n - i + 1) << (j == n - 1? "\n": " ");
+}
+
+int main()
+{
+
+//#ifndef ONLINE_JUDGE
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int test_case = 1;
+    // cin >> test_case;
+    for(int i = 1; i <= test_case; i++) solve(i);
+
+    return 0;
+}
