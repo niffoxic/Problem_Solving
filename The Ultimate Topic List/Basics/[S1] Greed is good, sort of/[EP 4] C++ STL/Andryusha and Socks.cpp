f50ced93c33eeda233455284fbaba4dc9fd708ba
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-02-19 20:45:13 +0530

    Create Andryusha and Socks.cpp

---
+++ The Ultimate Topic List/Basics/[S1] Greed is good, sort of/[EP 4] C++ STL/Andryusha and Socks.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+#define all(x) x.begin(), x.end()
+#define mkp make_pair
+#define pb push_back
+#define lb lower_bound
+#define up upper_bound
+
+
+const int mod = 1e9 + 7;
+const int dp_size = 1e5;
+const ll INF = LLONG_MAX;
+
+ll n, m, q, k, l, r, x, y, z, c;
+const ll template_array_size = 1e6;
+ll arr[template_array_size];
+ll brr[template_array_size];
+ll crr[template_array_size];
+
+ll Arr[1001][1001];
+ll Brr[1001][1001];
+ll Crr[1001][1001];
+
+string s, t;
+
+void solve(int case_no)
+{
+    cin >> n;
+    unordered_map<int, int> mp;
+    ll res = 0, c = 0;
+    for(int i = 0; i < 2 * n; i++){
+        cin >> q;
+        if (mp.find(q) == mp.end()) c++;
+        else c--;
+        mp[q]++;
+        res = max(res, c);
+    }
+    cout << res << "\n";
+}
+
+int main()
+{
+
+//#ifndef ONLINE_JUDGE
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int test_case = 1;
+    // cin >> test_case;
+    for(int i = 1; i <= test_case; i++) solve(i);
+
+    return 0;
+}
