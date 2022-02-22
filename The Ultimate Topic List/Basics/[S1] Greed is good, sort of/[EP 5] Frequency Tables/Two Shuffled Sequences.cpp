20a838c6949a5e38167e177aadf47e3d16b8e60d
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-02-22 10:46:05 +0530

    Create Two Shuffled Sequences.cpp

---
+++ The Ultimate Topic List/Basics/[S1] Greed is good, sort of/[EP 5] Frequency Tables/Two Shuffled Sequences.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+#define all(x) x.begin(), x.end()
+#define mkp make_pair
+#define pb push_back
+#define lb lower_bound
+#define up upper_bound
+
+
+const int mod = 1e9 + 7;
+const int dp_size = 1e5;
+const ll INF = LLONG_MAX;
+
+ll n, m, q, k, l, r, x, y, z, c;
+const ll template_array_size = 1e6;
+ll arr[template_array_size];
+ll brr[template_array_size];
+ll crr[template_array_size];
+
+ll Arr[1001][1001];
+ll Brr[1001][1001];
+ll Crr[1001][1001];
+
+string s, t;
+
+
+void solve(int case_no)
+{
+    cin >> n;
+    unordered_map<ll, int> mp; r = 1;
+    vector<ll> left, right;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i], mp[arr[i]]++;
+        if (mp[arr[i]] > 2) r = 0;
+        if (mp[arr[i]] == 1) left.push_back(arr[i]);
+        else right.push_back(arr[i]);
+    }
+    if (!r) cout << "NO\n";
+    else{
+        sort(all(left));
+        sort(all(right), greater<>());
+        cout << "YES\n" << left.size() << "\n";
+        for(auto i: left) cout << i << " ";
+        cout << "\n";
+        cout << right.size() << "\n";
+        for(auto i: right) cout << i << " ";
+    }
+}
+
+int main()
+{
+
+//#ifndef ONLINE_JUDGE
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int test_case = 1;
+    // cin >> test_case;
+    for(int i = 1; i <= test_case; i++) solve(i);
+
+    return 0;
+}
