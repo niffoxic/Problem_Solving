e82402adf65d5d4bdcbcdaa523cd8694cc71beee
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-02-22 15:34:49 +0530

    Create Game with Telephone Numbers.cpp

---
+++ The Ultimate Topic List/Basics/[S1] Greed is good, sort of/[~] Practice/Mashup 1/Game with Telephone Numbers.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+#define all(x) x.begin(), x.end()
+#define mkp make_pair
+#define pb push_back
+#define lb lower_bound
+#define up upper_bound
+
+
+const int mod = 1e9 + 7;
+const int dp_size = 1e5;
+const ll INF = LLONG_MAX;
+ll n, m, q, k, l, r, x, y, z, c, ti, a, b;
+const ll template_array_size = 1e6;
+ll arr[template_array_size];
+ll brr[template_array_size];
+ll crr[template_array_size];
+ll Arr[1001][1001];
+ll Brr[1001][1001];
+ll Crr[1001][1001];
+string s, t;
+void solve(int case_no)
+{
+    cin >> n >> s;
+    ll eights = (n - 11) >> 1;
+    ll others = eights;
+    string res;
+    for(int i = 0; i < n; i++){
+        if (s[i] == '8'){
+            if (eights) eights--;
+            else res += '8';
+        }else{
+            if (others) others--;
+            else res += s[i];
+        }
+    }
+    cout << (res[0] == '8'? "YES": "NO");
+}
+
+int main()
+{
+
+//#ifndef ONLINE_JUDGE
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int test_case = 1;
+    // cin >> test_case;
+    for(int i = 1; i <= test_case; i++) solve(i);
+    return 0;
+}
