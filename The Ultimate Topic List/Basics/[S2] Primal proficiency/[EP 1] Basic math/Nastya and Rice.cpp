604b9b52533742bcf322430e0286ef8d7ef95d1a
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-02-23 18:18:51 +0530

    Create Nastya and Rice.cpp

---
+++ The Ultimate Topic List/Basics/[S2] Primal proficiency/[EP 1] Basic math/Nastya and Rice.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+#define all(x) x.begin(), x.end()
+#define mkp make_pair
+#define pb push_back
+#define lb lower_bound
+#define up upper_bound
+
+
+const int mod = 1e9 + 7;
+const int dp_size = 1e5;
+const ll INF = LLONG_MAX;
+
+ll n, m, q, k, l, r, x, y, z, c, d, ti, a, b;
+const ll template_array_size = 1e6;
+ll arr[template_array_size];
+ll brr[template_array_size];
+ll crr[template_array_size];
+
+ll Arr[1001][1001];
+ll Brr[1001][1001];
+ll Crr[1001][1001];
+
+string s, t;
+
+
+void solve(int case_no)
+{
+    cin >> n >> a >> b >> c >> d;
+    l = n * (a - b), r = n  * (a + b);
+    if (r < c - d || l > c + d)
+        cout << "NO\n";
+    else cout << "YES\n";
+}
+
+int main()
+{
+
+//#ifndef ONLINE_JUDGE
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+//#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int test_case = 1;
+    cin >> test_case;
+    for(int i = 1; i <= test_case; i++) solve(i);
+
+    return 0;
+}
