8a018c241952806dc1f045e63da6c220ffa3fd5d
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-02-25 17:08:51 +0530

    Update GCD.cpp

--- Templates/C++_Templates/Number Theory/GCD.cpp
+++ Templates/C++_Templates/Number Theory/GCD.cpp
@@ -13,3 +13,16 @@ T gcd (T _a, T _b) {
     }
     return _a;
 }
+
+template <class T>
+T __GCD(T _a, T _b){
+    if (!_a || !_b) return _a | _b;
+    unsigned shift = __builtin_ctz(_a | _b);
+    _a >>= __builtin_ctz(_a);
+    do{
+        _b >>= __builtin_ctz(_b);
+        if (_a > _b) swap(_a, _b);
+        _b -= _a;
+    } while (_b);
+    return _a << shift;
+}
