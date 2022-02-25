b7f83cd2a604a6aca8fe7f843e2bca3471f31c8e
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-02-25 17:02:05 +0530

    Update GCD.cpp

--- Templates/C++_Templates/Number Theory/GCD.cpp
+++ Templates/C++_Templates/Number Theory/GCD.cpp
@@ -3,3 +3,13 @@ T gcd(T _a, T _b){
     if (_b == 0) return _b;
     return (_b, _a % _b);
 }
+
+
+template <class T>
+T gcd (T _a, T _b) {
+    while (_b) {
+        _a %= _b;
+        swap(_a, _b);
+    }
+    return _a;
+}
