a9a71205ad690f7f0d555627562f70b9f9ffb274
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-02-27 14:47:14 +0530

    Update Modular Arithmetic.cpp

--- Templates/C++_Templates/Number Theory/Modular Arithmetic.cpp
+++ Templates/C++_Templates/Number Theory/Modular Arithmetic.cpp
@@ -1,10 +1,102 @@
 
 // Calculate pow with mod
- const int mod = 1e9 + 7;
+const int mod = 1e9 + 7;
+int64_t MOD;
+using mint = Mint<MOD>;
+
 template<class T>
 T pow_mod(T x, T n, T m){
     if (!n) return 1;
     T temp = pow_mod(x, n / 2, m);
     if (n & 1) return temp * temp * x % mod;
     return temp * temp % mod;
+}
+
+
+template<const int64_t& MOD>
+struct Mint {
+    using T = typename decay<decltype(MOD)>::type; T v;
+    Mint(int64_t v = 0) { if(v < 0) v = v % MOD + MOD; if(v >= MOD) v %= MOD; this->v = T(v); }
+    Mint(uint64_t v) { if (v >= MOD) v %= MOD; this->v = T(v); }
+    Mint(int v): Mint(int64_t(v)) {}
+    Mint(unsigned v): Mint(uint64_t(v)) {}
+    explicit operator int() const { return v; }
+    explicit operator int64_t() const { return v; }
+    explicit operator uint64_t() const { return v; }
+    friend istream& operator>>(istream& in, Mint& m) { int64_t v_; in >> v_; m = Mint(v_); return in; }
+    friend ostream& operator<<(ostream& os, const Mint& m) { return os << m.v; }
+
+    static T inv(T a, T m) {
+        T g = m, x = 0, y = 1;
+        while(a != 0) {
+            T q = g / a;
+            g %= a; swap(g, a);
+            x -= q * y; swap(x, y);
+        } return x < 0? x + m : x;
+    }
+
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+#if !defined(_WIN32) || defined(_WIN64)
+        return unsigned(x % m);
+#endif // x must be less than 2^32 * m
+        unsigned x_high = unsigned(x >> 32), x_low = unsigned(x), quot, rem;
+        asm("divl %4\n" : "=a" (quot), "=d" (rem) : "d" (x_high), "a" (x_low), "r" (m));
+        return rem;
+    }
+
+    Mint inv() const { return Mint(inv(v, MOD)); }
+    Mint operator-() const { return Mint(v? MOD-v : 0); }
+    Mint& operator++() { v++; if(v == MOD) v = 0; return *this; }
+    Mint& operator--() { if(v == 0) v = MOD; v--; return *this; }
+    Mint operator++(int) { Mint a = *this; ++*this; return a; }
+    Mint operator--(int) { Mint a = *this; --*this; return a; }
+    Mint& operator+=(const Mint& o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
+    Mint& operator-=(const Mint& o) { v -= o.v; if (v < 0) v += MOD; return *this; }
+    Mint& operator*=(const Mint& o) { v = fast_mod(uint64_t(v) * o.v); return *this; }
+    Mint& operator/=(const Mint& o) { return *this *= o.inv(); }
+    friend Mint operator+(const Mint& a, const Mint& b) { return Mint(a) += b; }
+    friend Mint operator-(const Mint& a, const Mint& b) { return Mint(a) -= b; }
+    friend Mint operator*(const Mint& a, const Mint& b) { return Mint(a) *= b; }
+    friend Mint operator/(const Mint& a, const Mint& b) { return Mint(a) /= b; }
+    friend bool operator==(const Mint& a, const Mint& b) { return a.v == b.v; }
+    friend bool operator!=(const Mint& a, const Mint& b) { return a.v != b.v; }
+    friend bool operator<(const Mint& a, const Mint& b) { return a.v < b.v; }
+    friend bool operator>(const Mint& a, const Mint& b) { return a.v > b.v; }
+    friend bool operator<=(const Mint& a, const Mint& b) { return a.v <= b.v; }
+    friend bool operator>=(const Mint& a, const Mint& b) { return a.v >= b.v; }
+    Mint operator^(int64_t p) {
+        if(p < 0) return inv() ^ -p;
+        Mint a = *this, res{1}; while(p > 0) {
+            if(p & 1) res *= a;
+            p >>= 1; if(p > 0) a *= a;
+        } return res;
+    }
+};
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    int Q;
+    while(cin >> MOD >> Q, MOD) {
+        while(Q--) {
+            char op; mint a, b, ans;
+            cin >> a >> op >> b;
+
+            bool bad = op == '/' and gcd((int64_t)b, MOD) != 1;
+
+            if(op == '+')
+                ans = a + b;
+            else if(op == '-')
+                ans = a - b;
+            else if(op == '*')
+                ans = a * b;
+            else if(!bad)
+                ans = a / b;
+
+            if(bad) cout << -1 << '\n';
+            else cout << ans << '\n';
+        }
+    }
+
 }
