eddbe2949e9216272ca0ccc2c89d666e3c73eb98
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-03-07 19:17:35 +0530

    Update Gas Stations.cpp

--- HackerEarth/Algorithms/Graph/Depth First Search/Gas Stations.cpp
+++ HackerEarth/Algorithms/Graph/Depth First Search/Gas Stations.cpp
@@ -1,1 +1,20 @@
 // No idea why
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define template_size 1000005
+
+
+int main(){
+    int n, x; cin >> n >> x;
+    int res = 1;
+    for(int i = 0; i < n; i++){
+        int t; cin >> t;
+        if (x >= t) res++, x -= t;
+        else x = -1;
+    }
+    cout << res;
+    return 0;
+}
