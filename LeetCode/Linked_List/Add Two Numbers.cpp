47ee14f636c571fec66ccfd11b14748585c5a6ba
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-03-09 19:50:28 +0530

    Create Add Two Numbers.cpp

---
+++ LeetCode/Linked_List/Add Two Numbers.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        int extra = 0;
+        ListNode *temp, *head = new ListNode();
+        temp = head;
+        while (l1 or l2 or extra) {
+            if (l1) extra += l1->val, l1 = l1->next;
+            if (l2) extra += l2->val, l2 = l2->next;
+            temp->val = extra % 10;
+            extra /= 10;
+            if (l1 || l2 || extra) temp = temp->next = new ListNode();
+        }
+        return head;
+    }
+};
