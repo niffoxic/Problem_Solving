ee0431d76b52443da3b3c04c3c8eafdb355d4b1e
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-03-11 17:45:46 +0530

    Update Count Operations to Obtain Zero.cpp

--- LeetCode/Weekly Contest/280/Count Operations to Obtain Zero.cpp
+++ LeetCode/Weekly Contest/280/Count Operations to Obtain Zero.cpp
@@ -1,12 +1,17 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int countOperations(int num1, int num2) {
-    int mx, mn, res = 0;
-    while (num1 and num2) {
-        if (num1 > num2) mx = num1, mn = num2, num1 %= num2;
-        else mx = num2, mn = num1, num2 %= num1;
-        res += mx / mn;
+
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int res = 0;
+        while (num1 && num2) {
+            if (num1 >= num2) num1 -= num2;
+            else num2 -= num1;
+            res++;
+        }
+        return res;
     }
-    return res;
-}
+};
