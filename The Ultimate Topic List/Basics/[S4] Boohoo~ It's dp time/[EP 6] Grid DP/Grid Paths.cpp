dd4a05507334f2a41c0494f0bbd25d1b0682fbbe
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-03-20 01:17:01 +0530

    Create Grid Paths.cpp

---
+++ The Ultimate Topic List/Basics/[S4] Boohoo~ It's dp time/[EP 6] Grid DP/Grid Paths.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define template_size 1001
+string grid[template_size];
+int dp[template_size][template_size];
+const int mod = 1e9 + 7;
+int n;
+
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> grid[i];
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if (grid[i - 1][j - 1] == '.'){
+                if (i == 1 && j == 1) dp[i][j] = 1;
+                else dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % mod;
+            }
+        }
+    }
+    return cout << dp[n][n], 0;
+}
