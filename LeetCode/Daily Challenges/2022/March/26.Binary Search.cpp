19d3b133d483b9496568093656997508c5e28930
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-03-26 16:25:45 +0530

    Create 26.Binary Search.cpp

---
+++ LeetCode/Daily Challenges/2022/March/26.Binary Search.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right){
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+};
