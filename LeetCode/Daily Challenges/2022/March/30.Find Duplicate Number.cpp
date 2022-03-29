f2bea02666a76e65cb69bbb77b73ded9910c916b
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-03-29 15:48:34 +0530

    Create 30.Find Duplicate Number.cpp

---
+++ LeetCode/Daily Challenges/2022/March/30.Find Duplicate Number.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            if (nums[abs(nums[i]) - 1] < 0) return abs(nums[i]);
+            nums[abs(nums[i]) - 1] *= -1;
+        }
+        return -1;
+    }
+};
