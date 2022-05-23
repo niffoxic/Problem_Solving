cb2b75718937159ea84fc8e5379ba493eee8bb79
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-05-23 22:58:23 +0530

    Create Container With Most Water.py

---
+++ LeetCode/Arrays/Container With Most Water.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        res = 0
+        while left < right:
+            res = max(res, min(height[left], height[right]) * (right - left))
+            if height[left] > height[right]:
+                right -= 1
+            else:
+                left += 1
+        return res
