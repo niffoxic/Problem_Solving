f870cdcd21e07ad1e69af4718cd0c037f9643f12
Harsh Dubey <69251487+niffoxic@users.noreply.github.com>
2022-09-03 12:33:35 +0100

    Add files via upload

---
+++ CodeChef/Starters/53/Car Trip.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        cout << max(3000, n * 10) << "\n";
+    }
+    return 0;
+}
